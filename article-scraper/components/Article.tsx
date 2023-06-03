4170dd2e5c89ad7b3fa46b6eadfd70892e545730
Ed <83490268+Edward-Stalley@users.noreply.github.com>
2023-06-03 13:12:28 +0900

    design - loading icon
    added color-coded loading icons for each website when retrieving articles.

--- article-scraper/components/Article.tsx
+++ article-scraper/components/Article.tsx
@@ -17,7 +17,7 @@ export default function Article(props: {
 }) {
 
   const [showArticle, setShowArticle] = useState(false)
-  
+  const destructoid = props.url.includes("destructoid") ? "hover:bg-emerald-400 bg-emerald-400 " : "b-blue-200"
   const handleClick = async function (url: any) {
     setShowArticle((prevState) => !prevState)
     // const response = await fetch(`http://localhost:3000/api/article/${url}`);
@@ -25,12 +25,18 @@ export default function Article(props: {
   };
 
   return (
+
+    
+
+
 <div className="  ">
+
+
     <div 
     className={`      
 auto-cols-min h-30  justify-center items-center rounded-xl ml-5 mr-5 mb-5 
    ${props.url.includes("gameinformer") ? "hover:bg-rose-400 bg-rose-200 " : "b-blue-200"}
-   ${props.url.includes("destructoid") ? "hover:bg-emerald-400 bg-emerald-400 " : "b-blue-200"}
+   ${destructoid}
    ${props.url.includes("rockpapershotgun") ? "hover:bg-amber-400 bg-amber-200" : "b-blue-200"}
    ${props.url.includes("escapist") ? "hover:bg-sky-400 bg-sky-300 " : "b-blue-200"}
       `}>
@@ -41,8 +47,19 @@ auto-cols-min h-30  justify-center items-center rounded-xl ml-5 mr-5 mb-5
       objectFit: "cover",
       borderRadius: "0.75rem 0.75rem 0 0",
     }} className=" rounded-xl  " />    
+<div className="flex-row">
+  <div className="flex justify-between" >
+  <div className="flex p-2 justify-start  ">
+<button className="flex  items-center justify-center h-8 w-8 hover:text-slate-300 font-semibold text-slate-800 ">&#43;</button>
+</div>
+  <div className="flex  p-2 justify-end   ">
+  <Link className=" flex justify-center  items-center rounded-full h-8 w-8 hover:text-slate-300 font-semibold text-slate-800" href={props.url}>&#8680;</Link>
+  </div>
+  </div>
+
 
   <h1 className="p-5 font-bold text-slate-800 ">{props.title}</h1>
+</div>
      
     
     <button onClick={handleClick} className={`
--- article-scraper/pages/index.tsx
+++ article-scraper/pages/index.tsx
@@ -13,21 +13,37 @@ import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
+
+
 export default function Home() {
+
+
+
+
   const [retrievedArticles, setRetrievedArticles] = useState<Object>([]);
   const [finalArticles, setFinalArticles] = useState<Object>([]);
   const [fetched, setFetched] = useState(false);
   const [href, setHref] = useState("");
   const [post, setPost] = useState("");
   const [loading, setLoading] = useState(false);
+  const [isDestructoid, setIsDestructoid] = useState(false)
+  const [isEscapist, setIsEscapist] = useState(false)
+  const [isRPS, setIsRPS] = useState(false)
+  const [isGameInformer, setIsGameInformer] = useState(false)
+
   // const[showArticle, setShowArticle] = useState(false)
   // const [urls, setUrls] = useState<Object>([]);
 
   const getUrl = async (url: string) => {
+    setLoading(true)
+    console.log(loading)
     const res = await fetch(`/api/${url}`);
     const { articles } = await res.json();
     setRetrievedArticles(articles);
     setFetched((fetched) => !fetched);
+    setLoading((prevState) => !prevState)
+    console.log(loading)
+
   };
 
   const urls: any[] = [];
@@ -38,10 +54,18 @@ export default function Home() {
     setHref(href);
   };
 
+
+
   const getArticleBody = async (url: string) => {
     const res = await fetch(`/api/puppeteer/rockpapershotgun`);
   };
 
+  
+const checkStyle = function(url: string | string[], site:string ){
+  url.includes(site) 
+  return `styles.${site}`
+
+}
 
   useEffect(() => {
     const toComponent =
@@ -54,8 +78,12 @@ export default function Home() {
 Want to add
 
 1.  book icon for reading 
-2. + icon for bookmarking
-3. functionality on click - show/hide/save*/}
+2. + icon for bookmarking [done] !! not functional though
+3. functionality on click - show/hide [done]
+4. functionality for save
+5. color coded - loading button [done]
+
+*/}
 
 {/* <button onClick={()=>setShowArticle(!showArticle)} className="bg-emerald-200 p-3 m-1">Read Article</button> */}
 {/* <button className="bg-green-100 p-3 m-1">Add Article</button> */}
@@ -80,17 +108,7 @@ Want to add
     }} className=" rounded pb-0 px-5 " /> */}
                   </div>
 </div> 
-                <div
-                  className={`      
-                   auto-cols-min h-30  justify-center items-center rounded-b-xl ml-5 mr-5 mb-5 
-                  ${a.url.includes("gameinformer") ? "hover:bg-rose-400 bg-rose-200 " : "b-blue-200"}
-                  ${a.url.includes("destructoid") ? "hover:bg-emerald-400 bg-emerald-200 " : "b-blue-200"}
-                  ${a.url.includes("rockpapershotgun") ? "hover:bg-amber-400 bg-amber-200" : "b-blue-200"}
-                  ${a.url.includes("escapist") ? "hover:bg-sky-400 bg-sky-300 " : "b-blue-200"}
-            `}
-                >
-        
-                </div>
+         
               </Link>
             </div>
           );
@@ -115,7 +133,10 @@ Want to add
             <button
               onClick={() => {
                 getUrl("rockpapershotgun");
-              }}
+                setIsRPS(true)       
+                setIsGameInformer(false)
+                setIsDestructoid(false)
+                setIsEscapist(false)       }}
               className="bg-amber-400 p-5 sm:col-span-2 rounded-xl  sm:p-5  hover:bg-slate-600"
             >
               R.P.S
@@ -124,6 +145,11 @@ Want to add
             <button
               onClick={() => {
                 getUrl("gameinformer");
+                setIsGameInformer(true)
+                setIsDestructoid(false)
+                setIsEscapist(false)
+                setIsRPS(false)
+              
               }}
               className="rounded-xl p-5   bg-rose-400    hover:bg-slate-600"
             >
@@ -132,6 +158,10 @@ Want to add
             <button
               onClick={() => {
                 getUrl("destructoid");
+                setIsDestructoid(true)
+                setIsEscapist(false)
+                setIsGameInformer(false)
+                setIsRPS(false)
               }}
               className="rounded-xl p-5  bg-emerald-300 hover:bg-emerald-400"
             >
@@ -140,6 +170,12 @@ Want to add
             <button
               onClick={() => {
                 getUrl("escapist");
+                setIsEscapist(true)
+                setIsDestructoid(false)
+                setIsGameInformer(false)
+                setIsRPS(false)
+                
+
               }}
               className="rounded-xl p-5 sm:col-span-2  bg-sky-300  hover:bg-sky-400"
             >
@@ -147,11 +183,33 @@ Want to add
             </button>
           </div>
         </div>
+        {/* need to fix this logic */}
+  {!loading? 
+
        <div className="flex sm:w-full w-64 max-w-3xl justify-center items-center py-1 mt-20">
   <div className="w-full h-full max-w-3xl">
     <Carousel items={finalArticles} />
   </div>
+</div> :
+
+<div role="status">
+    <svg aria-hidden="true"  className={ `
+    ${isDestructoid && styles.destructoid }
+    ${isEscapist && styles.escapist }
+    ${isGameInformer && styles.gameInformer }
+    ${isRPS && styles.rps }
+
+
+    w-8 h-8 mr-2 mt-20 text-gray-200 animate-spin dark:text-gray-600 `} viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span className="sr-only">Loading...</span>
 </div>
+
+
+
+  }
 
         </div>
         
@@ -162,41 +220,6 @@ Want to add
 
 
 // old code
-
-{/* <div className="grid w-100 grid-cols-1 sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-1 mt-10"> */}
-        {/* <Carousel items={finalArticles}/> */}
-          {/* <> {finalArticles}</> */}
-      {/* </div> */}
-
-        {/* <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 mt-10"> */}
-        {/* </div> */}
-
-                  {/* <Link */}
-                  {/* // className="bg-orange-300 text-slate-800 text-xl p-3 rounded-b-xl flex justify-center
-                  hover:bg-orange-400" // href={a.url} */}
-                  {/* > */}
-                  {/* Read */}
-                  {/* </Link> */}
-                  {/* Need to implement getting the body from article */}
-                  {/* 
-                <button
-                  className="bg-orange-500  w-full flex justify-center p-4 rounded-b-xl"
-                  data-key={i}
-                  data-href={a.url}
-                  onClick={() => {
-                    getArticleBody(a.url);
-                  }}
-                >
-                  Handler
-                </button> */}
-                  {/* <div className="flex justify-center    ">
-                    {(a.url.includes("rockpapershotgun") && "Rock Paper Shotgun") ||
-                      (a.url.includes("destructoid") && "Destructoid") ||
-                      (a.url.includes("destructoid") && "Destructoid")}
-                  </div> */}
-
-
-                  // -----------------IGN
 
 
                        {/* <button
